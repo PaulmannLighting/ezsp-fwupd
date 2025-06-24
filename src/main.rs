bcddf77aa43c4f4a91bf3a7f5fe8cc23fdf2aafe
Richard Neumann <neumann@paulmann.de>
2025-06-24 08:13:40 +0200

    Add base implementation for current silabs chip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+pub use firmware_updater::FirmwareUpdater;
+
 mod firmware_updater;
+mod silabs;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/silabs.rs
@@ -0,0 +1,30 @@
+use std::path::PathBuf;
+
+use semver::Version;
+
+use crate::FirmwareUpdater;
+
+/// Represents the Silicon Labs MGM210P22A device.
+pub struct MGM210P22A {
+    tty: PathBuf,
+}
+
+impl FirmwareUpdater for MGM210P22A {
+    type Version = Version;
+
+    fn current_version(&self) -> Self::Version {
+        todo!()
+    }
+
+    fn latest_version(&self) -> Option<Self::Version> {
+        todo!()
+    }
+
+    fn available_versions(&self) -> Vec<Self::Version> {
+        todo!()
+    }
+
+    fn install_version(&self, version: Self::Version) -> std::io::Result<()> {
+        todo!()
+    }
+}
