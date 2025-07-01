e119bfcad2569917f31b67e242a7143181a0e9ad
Richard Neumann <neumann@paulmann.de>
2025-07-01 08:36:15 +0200

    Outsource frame sending to sealed trait

--- src/xmodem/send.rs
+++ src/xmodem/send.rs
@@ -1,16 +1,16 @@
-use std::io::{ErrorKind, Read, Write};
-
-use ashv2::HexSlice;
 use indicatif::ProgressBar;
-use log::{debug, trace};
+use log::debug;
 
-use super::frame::{ACK, EOT, Frame, NAK};
+use super::frame::EOT;
 use super::frames::Frames;
 use crate::ignore_timeout::IgnoreTimeout;
+use send_frame::SendFrame;
+
+mod send_frame;
 
 const MAX_RETRIES: usize = 10;
 
-pub trait Send: Read + Write {
+pub trait Send: SendFrame {
     /// Sends a file using the XMODEM protocol.
     fn send<T>(&mut self, data: T, progress_bar: Option<&ProgressBar>) -> std::io::Result<Box<[u8]>>
     where
@@ -32,57 +32,6 @@ pub trait Send: Read + Write {
         self.read_to_end(&mut buffer).ignore_timeout()?;
         Ok(buffer.into_boxed_slice())
     }
-
-    fn send_frame(&mut self, index: usize, frame: Frame) -> std::io::Result<()> {
-        debug!("Sending frame #{index}...");
-
-        let mut ctr: usize = 0;
-        let bytes = frame.into_bytes();
-        trace!("Sending frame #{index}: {:#04X}", HexSlice::new(&bytes));
-
-        loop {
-            match self.try_send_frame(&bytes) {
-                Ok(()) => return Ok(()),
-                Err(error) => {
-                    if ctr >= MAX_RETRIES {
-                        return Err(std::io::Error::new(
-                            ErrorKind::TimedOut,
-                            "Maximum retries exceeded",
-                        ));
-                    }
-
-                    debug!("Attempt {ctr} failed: {error}, retrying...");
-                }
-            }
-
-            ctr += 1;
-        }
-    }
-
-    fn try_send_frame(&mut self, frame: &[u8]) -> std::io::Result<()> {
-        self.write_all(frame)?;
-        self.flush()?;
-
-        let mut response = [0];
-        self.read_exact(&mut response)?;
-        trace!("Received {response:#02X?}");
-        let [byte] = response;
-        let mut excess = Vec::new();
-        self.read_to_end(&mut excess).ignore_timeout()?;
-        trace!(
-            "Received {} excess bytes: {:#04X}",
-            excess.len(),
-            HexSlice::new(&excess)
-        );
-
-        match byte {
-            ACK => Ok(()),
-            NAK => Err(std::io::Error::other("NAK received, retransmitting frame")),
-            other => Err(std::io::Error::other(format!(
-                "Received unexpected response: {other}"
-            ))),
-        }
-    }
 }
 
-impl<T> Send for T where T: Read + Write {}
+impl<T> Send for T where T: SendFrame {}
---
+++ src/xmodem/send/send_frame.rs
@@ -0,0 +1,64 @@
+use std::io::{ErrorKind, Read, Write};
+
+use ashv2::HexSlice;
+use log::{debug, trace};
+
+use crate::ignore_timeout::IgnoreTimeout;
+use crate::xmodem::frame::{ACK, Frame, NAK};
+use crate::xmodem::send::MAX_RETRIES;
+
+/// Sealed trait for sending XMODEM frames.
+pub trait SendFrame: Read + Write {
+    fn send_frame(&mut self, index: usize, frame: Frame) -> std::io::Result<()> {
+        debug!("Sending frame #{index}...");
+
+        let mut ctr: usize = 0;
+        let bytes = frame.into_bytes();
+        trace!("Sending frame #{index}: {:#04X}", HexSlice::new(&bytes));
+
+        loop {
+            match self.try_send_frame(&bytes) {
+                Ok(()) => return Ok(()),
+                Err(error) => {
+                    if ctr >= MAX_RETRIES {
+                        return Err(std::io::Error::new(
+                            ErrorKind::TimedOut,
+                            "Maximum retries exceeded",
+                        ));
+                    }
+
+                    debug!("Attempt {ctr} failed: {error}, retrying...");
+                }
+            }
+
+            ctr += 1;
+        }
+    }
+
+    fn try_send_frame(&mut self, frame: &[u8]) -> std::io::Result<()> {
+        self.write_all(frame)?;
+        self.flush()?;
+
+        let mut response = [0];
+        self.read_exact(&mut response)?;
+        trace!("Received {response:#02X?}");
+        let [byte] = response;
+        let mut excess = Vec::new();
+        self.read_to_end(&mut excess).ignore_timeout()?;
+        trace!(
+            "Received {} excess bytes: {:#04X}",
+            excess.len(),
+            HexSlice::new(&excess)
+        );
+
+        match byte {
+            ACK => Ok(()),
+            NAK => Err(std::io::Error::other("NAK received, retransmitting frame")),
+            other => Err(std::io::Error::other(format!(
+                "Received unexpected response: {other}"
+            ))),
+        }
+    }
+}
+
+impl<T> SendFrame for T where T: Read + Write {}
