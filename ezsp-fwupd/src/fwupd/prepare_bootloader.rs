b0ac94baa846fd87101b313cd0be21fa290367e0
Richard Neumann <neumann@paulmann.de>
2025-11-20 09:22:56 +0100

    Always return `Self` from `prepare_bootloader()`.

--- ezsp-fwupd/src/fwupd/prepare_bootloader.rs
+++ ezsp-fwupd/src/fwupd/prepare_bootloader.rs
@@ -1,3 +1,5 @@
+use std::io::Error;
+
 use ezsp::uart::Uart;
 use ezsp::{Bootloader, Callback};
 use log::{debug, error};
@@ -13,14 +15,14 @@ pub trait PrepareBootloader: Sized {
     /// # Errors
     ///
     /// Returns an [`std::io::Error`] if the operation fails.
-    fn prepare_bootloader(self) -> impl Future<Output = std::io::Result<Self>>;
+    fn prepare_bootloader(self) -> impl Future<Output = Self>;
 }
 
 impl<T> PrepareBootloader for T
 where
     T: SerialPort + 'static,
 {
-    async fn prepare_bootloader(self) -> std::io::Result<Self> {
+    async fn prepare_bootloader(self) -> Self {
         let (callbacks_tx, _callbacks_rx) = channel::<Callback>(8);
         let mut uart = Uart::new(self, callbacks_tx, 8, 8);
 
@@ -30,6 +32,6 @@ where
             .unwrap_or_else(|error| {
                 error!("Failed to launch standalone bootloader: {error}");
             });
-        Ok(uart.terminate())
+        uart.terminate()
     }
 }
