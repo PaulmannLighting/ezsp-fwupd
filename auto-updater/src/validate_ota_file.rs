41335a7d7f5e342eb2865d580e42a54adfade5f8
Richard Neumann <neumann@paulmann.de>
2026-02-23 12:48:00 +0100

    Simplify OTA file loading.

--- auto-updater/src/validate_ota_file.rs
+++ auto-updater/src/validate_ota_file.rs
@@ -8,27 +8,19 @@ use crate::manifest::Metadata;
 
 /// Validate the OTA file by reading it and checking its contents.
 pub fn validate_ota_file(metadata: &Metadata) -> Option<OtaFile> {
-    let Ok(ota_file) = read(metadata.filename())
+    let ota_file_bytes = read(metadata.filename())
         .inspect_err(|error| error!("Failed to read firmware file: {error}"))
-    else {
-        return None;
-    };
+        .ok()?;
 
-    let Ok(ota_file) = OtaFile::from_le_stream_exact(ota_file.into_iter())
+    let ota_file = OtaFile::from_le_stream_exact(ota_file_bytes.into_iter())
         .inspect_err(|error| error!("Failed to parse OTA file: {error}"))
-        .map_err(drop)
-        .and_then(|ota_file| {
-            ota_file
-                .validate()
-                .inspect_err(|error| {
-                    error!("Invalid OTA file magic: {error:#04X?}");
-                })
-                .map_err(drop)
-        })
-    else {
-        return None;
-    };
+        .ok()?;
 
     ota_file.header().log();
-    Some(ota_file)
+    ota_file
+        .validate()
+        .inspect_err(|error| {
+            error!("Invalid OTA file magic: {error:#04X?}");
+        })
+        .ok()
 }
